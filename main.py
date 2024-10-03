3f133e326c726c3eb9a60cacf96c2f917dcac1e3
TorReto692 <tikhonovdaminik@gmail.com>
2024-10-03 12:14:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-day = int(input("Введите день: ")) 
-month = int(input("Введите месяц: ")) 
-if month < 1 or month>12:
-    print("Ошибка месяца")
-elif day<1 or day>31: 
+day = int(input("Введите день: ")) # Запрашиваем ввод числа дня.
+month = int(input("Введите месяц(числом): ")) # Запрашиваем ввод числа месяца.
+if month<1 or month>12: # Проверяем на корректный месяц.
+    print("Ошибка месяца") # Выводим что у нас некорректный месяц.
+elif day<1 or day>31: # Проверяем на корректный день.
     print ("Некорректный день")
 else:
     if (month == 3 and day >= 1) or (month == 4) or (month == 5 and day <= 31):
-        elif (month == 6 and day >= 1) or (month == 7) or (month == 8) or (month == 8 and day <= 31): 
+        season = "Весна" # Присваиваем сезон "Весна".
+    elif (month == 6 and day >= 1) or (month == 7) or (month == 8) or (month == 8 and day <= 31):
+        season = "Лето" # Присваиваем сезон "Лето".
     elif (month == 9 and day >= 1) or (month == 10) or (month == 11 and day <= 30): 
-else:
+        season = "Осень" # Присваиваем сезон "Осень".
+    else: 
+        season = "Зима" # Присваиваем сезон "Зима".
     print(f"Это: {season}") 

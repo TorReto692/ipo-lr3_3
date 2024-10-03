33a0eee6db76a36ee77a7892afc7a4cc58a0544d
TorReto692 <tikhonovdaminik@gmail.com>
2024-10-03 12:05:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-day = int(input("Введите день: "))
-month = int(input("Введите месяц: "))
-
-if (month == 3 and day >= 1) or (month == 4) or (month == 5) or (month == 6 and day <= 31):
-    season = "Весна"
-elif (month == 6 and day >= 1) or (month == 7) or (month == 8) or (month == 8 and day <= 31):
-    season = "Лето"
-elif (month == 9 and day >= 1) or (month == 10) or (month == 11 and day <= 30):
-    season = "Осень"
+day = int(input("Введите день: ")) 
+month = int(input("Введите месяц: ")) 
+if month < 1 or month>12:
+    print("Ошибка месяца")
+elif day<1 or day>31: 
+    print ("Некорректный день")
 else:
-    season = "Зима"
-
-print(f"Дата: {day}.{month} относится к сезону: {season}")
+    if (month == 3 and day >= 1) or (month == 4) or (month == 5 and day <= 31):
+        elif (month == 6 and day >= 1) or (month == 7) or (month == 8) or (month == 8 and day <= 31): 
+    elif (month == 9 and day >= 1) or (month == 10) or (month == 11 and day <= 30): 
+else:
+    print(f"Это: {season}") 
